d9f3122432281a38f75ce210214499c2a959e9dd
Ael <laelsirus@gmail.com>
2019-06-22 17:43:32 +0900

    Update sketch.js

--- example/sketch.js
+++ example/sketch.js
@@ -1,32 +1,10 @@
-function setup() {
-  title = createElement('h2', "<a href='/PoeticCodeForNature'> HOME : </a> 작품 제목!");
-  title.position(20, 0);
-
-  canvas = createCanvas(300, 300);
-  canvas.position(20, 60);
-  canvas.class("artwork");
-
-  description = "\
-  작품에 대한 설명이 들어갑니다. <br/> \
-  HTML이 직접 들어가서 줄넘김을 할 수 있습니다. \
-  ";
-  text = createDiv(description);
-  text.position(20, 400);
-  text.style("font-family", "monospace");
-  text.style("font-size", "12pt");
-
-}
-
-// let walker;
-// let a;
-
 // function setup() {
-//   title = createElement('h2', "<a href='/PoeticCodeForNature'> HOME : </a> 작품 제목!);
+//   title = createElement('h2', "<a href='/PoeticCodeForNature'> HOME : </a> 작품 제목!");
 //   title.position(20, 0);
 
-//   createCanvas(640,360);
-//   walker = new Walker(a);
-//   background(127);
+//   canvas = createCanvas(300, 300);
+//   canvas.position(20, 60);
+//   canvas.class("artwork");
 
 //   description = "\
 //   작품에 대한 설명이 들어갑니다. <br/> \
@@ -36,40 +14,62 @@ function setup() {
 //   text.position(20, 400);
 //   text.style("font-family", "monospace");
 //   text.style("font-size", "12pt");
+
 // }
 
-// function draw() {
-//   if (mouseIsPressed){
-//     walker.step(1);
-//     walker.render(1);
-//   }
-// }
+let walker;
+let a;
+
+function setup() {
+  title = createElement('h2', "<a href='/PoeticCodeForNature'> HOME : </a> 작품 제목12");
+  title.position(20, 0);
+
+  createCanvas(640,360);
+  walker = new Walker(a);
+  background(127);
+
+  description = "\
+  작품에 대한 설명이 들어갑니다. <br/> \
+  HTML이 직접 들어가서 줄넘김을 할 수 있습니다. \
+  ";
+  text = createDiv(description);
+  text.position(20, 400);
+  text.style("font-family", "monospace");
+  text.style("font-size", "12pt");
+}
+
+function draw() {
+  if (mouseIsPressed){
+    walker.step(1);
+    walker.render(1);
+  }
+}
 
 
 
-// class Walker {
-//   constructor(){
-//     this.x = random(0, width);
-//     this.y = height/2;
-//   }
+class Walker {
+  constructor(){
+    this.x = random(0, width);
+    this.y = height/2;
+  }
 
-//   render() {
-//     stroke(50);
-//     point(this.x,this.y);
-//   }
+  render() {
+    stroke(50);
+    point(this.x,this.y);
+  }
 
-//   step() {
-//     var choice = floor(random(6));
-//     if (choice == 0 && 1) {
-//       this.x++;
-//     } else if (choice == 2 && 3) {
-//       this.x--;
-//     } else if (choice == 4) {
-//       this.y++;
-//     } else {
-//       this.y--;
-//     }
-//     this.x = constrain(this.x,0,width-1);
-//     this.y = constrain(this.y,0,height-1);
-//   }
-// }
+  step() {
+    var choice = floor(random(6));
+    if (choice == 0 && 1) {
+      this.x++;
+    } else if (choice == 2 && 3) {
+      this.x--;
+    } else if (choice == 4) {
+      this.y++;
+    } else {
+      this.y--;
+    }
+    this.x = constrain(this.x,0,width-1);
+    this.y = constrain(this.y,0,height-1);
+  }
+}

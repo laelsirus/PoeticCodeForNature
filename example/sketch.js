f80ab19e2b5ee87e788be0add971968444afbe20
Ael <laelsirus@gmail.com>
2019-06-22 17:38:09 +0900

    Update sketch.js

--- example/sketch.js
+++ example/sketch.js
@@ -1,10 +1,32 @@
+// function setup() {
+//   title = createElement('h2', "<a href='/PoeticCodeForNature'> HOME : </a> 작품 제목");
+//   title.position(20, 0);
+
+//   canvas = createCanvas(300, 300);
+//   canvas.position(20, 60);
+//   canvas.class("artwork");
+
+//   description = "\
+//   작품에 대한 설명이 들어갑니다. <br/> \
+//   HTML이 직접 들어가서 줄넘김을 할 수 있습니다. \
+//   ";
+//   text = createDiv(description);
+//   text.position(20, 400);
+//   text.style("font-family", "monospace");
+//   text.style("font-size", "12pt");
+
+// }
+
+let walker;
+let a;
+
 function setup() {
   title = createElement('h2', "<a href='/PoeticCodeForNature'> HOME : </a> 작품 제목");
   title.position(20, 0);
 
-  canvas = createCanvas(300, 300);
-  canvas.position(20, 60);
-  canvas.class("artwork");
+  createCanvas(640,360);
+  walker = new Walker(a);
+  background(127);
 
   description = "\
   작품에 대한 설명이 들어갑니다. <br/> \
@@ -14,16 +36,6 @@ function setup() {
   text.position(20, 400);
   text.style("font-family", "monospace");
   text.style("font-size", "12pt");
-
-}
-
-let walker;
-let a;
-
-function setup() {
-  createCanvas(640,360);
-  walker = new Walker(a);
-  background(127);
 }
 
 function draw() {

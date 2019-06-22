f2112ebeeced74ba314ad8561d3322acddabf782
Yuhwa <yuhwa@Yuhwa-MacBookPro.local>
2019-06-22 23:07:17 +0900

    Update sketch.js

--- example/sketch.js
+++ example/sketch.js
@@ -26,11 +26,11 @@ let upDown
 //let wall;
 
 function setup() {
-  title = createElement('h2', "<a href='/PoeticCodeForNature'> HOME : </a> 작품 제목12");
+  title = createElement('h2', "<a href='/PoeticCodeForNature'> HOME : </a> 반디");
   title.position(20, 0);
   description = "\
-  작품에 대한 설명이 들어갑니다. <br/> \
-  HTML이 직접 들어가서 줄넘김을 할 수 있습니다. \
+  작품설명 <br/> \
+  작품설명 \
   ";
   text = createDiv(description);
   text.position(20, 400);

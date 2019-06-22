cd03bedb2e8915c51b7a538f2d902371101af415
Yuhwa <yuhwa@Yuhwa-MacBookPro.local>
2019-06-22 21:43:45 +0900

    Update sketch.js

--- example/sketch.js
+++ example/sketch.js
@@ -18,7 +18,9 @@
 // }
 
 let walker;
-let a;
+let stepProb;
+let randomColor;
+let randomStroke;
 
 function setup() {
   title = createElement('h2', "<a href='/PoeticCodeForNature'> HOME : </a> 작품 제목12");
@@ -27,8 +29,7 @@ function setup() {
   createCanvas(640,360);
   walker = new Walker();
   walker2 = new Walker();
-  background(127);
-  mouseStartX = width/2;
+  background(0);
 
   description = "\
   작품에 대한 설명이 들어갑니다. <br/> \
@@ -40,9 +41,15 @@ function setup() {
   text.style("font-size", "12pt");
 }
 
+
 function draw() {
+  background(0, 10);
   if (mouseIsPressed){
     randomColor = random (0, 200);
+    randomStroke = random (0, 50);
+    stepProb = 9;
+    
+    
     walker.step(6);
     walker.render(randomColor);
     
@@ -56,26 +63,30 @@ function draw() {
 class Walker {
   constructor(){
     this.x = random (0, width);
-    this.y = height;
+    this.y = 0;
   }
 
   render() {
-    stroke(randomColor);
+    strokeWeight(randomStroke);
+    stroke(255, randomColor, randomColor, 10);
     point(this.x,this.y);
   }
 
-  step(stepProb) {
-    var choice = floor(random(stepProb));
-    if (choice == 0 && 1) {
-      this.x++;
-    } else if (choice == 2 && 3) {
-      this.x--;
-    } else if (choice == 4) {
-      this.y++;
-    } else {
-      this.y--;
-    }
-    this.x = constrain(this.x,0,width-1);
-    this.y = constrain(this.y,0,height-1);
+  step() { 
+      choice = floor(random(0, stepProb));
+      if (choice == 0 && 1) {
+        this.x++;
+      }
+      else if (choice == 2 && 3) {
+        this.x--;
+      }
+      else if (choice == 4) {
+        this.y--;
+      }
+      else if (choice > 4) {
+        this.y++;
+      }
+    // this.x = constrain(this.x,0,width-1);
+    // this.y = constrain(this.y,0,height-1);
   }
 }
